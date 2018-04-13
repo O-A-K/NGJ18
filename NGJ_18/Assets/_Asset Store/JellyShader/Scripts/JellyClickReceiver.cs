447848594a931f3d3019cae5ec80670e10754e1c
Splogu <sean_p_logue@hotmail.co.uk>
2018-04-13 21:43:15 +0200

    Added Asset Store Stuff
    Asset stuff

---
+++ NGJ_18/Assets/_Asset Store/JellyShader/Scripts/JellyClickReceiver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyClickReceiver : MonoBehaviour {
+
+    RaycastHit hit;
+    Ray clickRay;
+
+    Renderer modelRenderer;
+    float controlTime;
+
+	// Use this for initialization
+	void Start () {
+        modelRenderer = GetComponent<MeshRenderer>();
+
+        InvokeRepeating("JellyWobble", 1, 1);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        controlTime += Time.deltaTime;
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(clickRay, out hit))
+            {
+                controlTime = 0;
+
+                modelRenderer.material.SetVector("_ModelOrigin", transform.position);
+                modelRenderer.material.SetVector("_ImpactOrigin", hit.point);
+            }
+        }
+
+        modelRenderer.material.SetFloat("_ControlTime", controlTime);*/
+	}
+
+    private void JellyWobble()
+    {
+        controlTime = 0;
+
+        modelRenderer.material.SetVector("_ModelOrigin", transform.position);
+        modelRenderer.material.SetVector("_ImpactOrigin", Vector3.zero);
+
+        modelRenderer.material.SetFloat("_ControlTime", controlTime);
+    }
+
+}
