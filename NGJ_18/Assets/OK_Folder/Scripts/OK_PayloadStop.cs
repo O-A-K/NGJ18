2fbfcb9d8bea12f53e4a441a90243aeaccbc3879
Oliver Knight <oliknight95@gmail.com>
2018-04-14 20:40:31 +0100

    Cart Stop Starter

---
+++ NGJ_18/Assets/OK_Folder/Scripts/OK_PayloadStop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OK_PayloadStop : MonoBehaviour {
+
+    public GameObject GO_Cart;
+    
+    // Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    private void OnTriggerEnter(Collider cl_other)
+    {
+        GO_Cart.GetComponent<Battlehub.SplineEditor.SplineFollow>().IsRunning = false;
+    }
+
+    private void OnTriggerExit(Collider cl_other)
+    {
+        GO_Cart.GetComponent<Battlehub.SplineEditor.SplineFollow>().IsRunning = true;
+    }
+}
