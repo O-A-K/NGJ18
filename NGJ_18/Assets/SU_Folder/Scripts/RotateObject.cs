b34a942ac0cb02df7cf00511a5375669f8879f7f
Unknown <sven.ulrich87@googlemail.com>
2018-04-15 02:10:48 +0100

    Added and updated models! Yeha

---
+++ NGJ_18/Assets/SU_Folder/Scripts/RotateObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    public bool rot_x;
+    public bool rot_y;
+    public bool rot_z;
+    public float speed = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (rot_x == true)
+        {
+            transform.Rotate(speed, 0, 0);
+        }
+        else if(rot_y == true)
+        {
+            transform.Rotate(0, speed, 0);
+        }
+        else if(rot_z == true)
+        {
+            transform.Rotate(0, 0, speed);
+        }
+	}
+}
